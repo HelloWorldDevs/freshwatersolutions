d6ae5b5aba4467fa5409e902df2d381a861fa780
Keenan <k.p.wellsy@gmail.com>
2016-05-04 10:08:06 -0700

    added contact form7 plugin and text styles to child theme

--- wp-content/themes/Avada-Child-Theme/functions.php
+++ wp-content/themes/Avada-Child-Theme/functions.php
@@ -1,8 +1,14 @@
 <?php
 
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'avada-parent-stylesheet', get_template_directory_uri() . '/style.css' );
+    $parent_style = 'parent-style';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    array( $parent_style )
+   );
 }
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 function avada_lang_setup() {
@@ -10,3 +16,5 @@ function avada_lang_setup() {
 	load_child_theme_textdomain( 'Avada', $lang );
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
+
+?>
