c22a073cf5a6bc178e8b100e0abb0354e0b774fa
Keenan Wells <Keenan@Keenans-MacBook-Pro.local>
2016-09-07 14:06:33 -0700

    added urls for test and live site in index.js of child avada theme.

--- wp-content/themes/Avada-Child-Theme/index.js
+++ wp-content/themes/Avada-Child-Theme/index.js
@@ -4,12 +4,27 @@ jQuery(window).load(function() {
   var hash = '#' + url.split('#')[1];
   var tag = '.' + url.split('#')[1];
   var baseUrl = url.split('#')[0];
+
   if(url === 'http://fws.dev/portfolio/' + hash){
     setTimeout(function(){
       jQuery('.fusion-portfolio-wrapper').isotope({ filter: tag });
     }, 700);
   }
+
+  if(url === 'http://fwsol.bfdig.com/portfolio/' + hash){
+    setTimeout(function(){
+      jQuery('.fusion-portfolio-wrapper').isotope({ filter: tag });
+    }, 700);
+  }
 });
+
+if(url === 'http://freshwatersim.com/portfolio/' + hash){
+  setTimeout(function(){
+    jQuery('.fusion-portfolio-wrapper').isotope({ filter: tag });
+  }, 700);
+}
+});
+
 
 //#web-applications
 
