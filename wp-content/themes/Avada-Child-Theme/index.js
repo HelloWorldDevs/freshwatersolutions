27403c98c74bbf9e4d86573c335eba854eb2404e
Keenan Wells <Keenan@Keenans-MacBook-Pro.local>
2016-09-08 08:59:01 -0700

    portfolio categories isotope tag now independent of url

--- wp-content/themes/Avada-Child-Theme/index.js
+++ wp-content/themes/Avada-Child-Theme/index.js
@@ -1,30 +1,17 @@
 
 jQuery(window).load(function() {
   var url = window.location.href;
-  var hash = '#' + url.split('#')[1];
   var tag = '.' + url.split('#')[1];
-  var baseUrl = url.split('#')[0];
+  var pathname = window.location.pathname;
 
-  if(url === 'http://fws.dev/portfolio/' + hash){
+  if(pathname === '/portfolio/'){
+    console.log('works!')
     setTimeout(function(){
       jQuery('.fusion-portfolio-wrapper').isotope({ filter: tag });
     }, 700);
   }
 
-  if(url === 'http://fwsol.bfdig.com/portfolio/' + hash){
-    setTimeout(function(){
-      jQuery('.fusion-portfolio-wrapper').isotope({ filter: tag });
-    }, 700);
-  }
 });
-
-if(url === 'http://freshwatersim.com/portfolio/' + hash){
-  setTimeout(function(){
-    jQuery('.fusion-portfolio-wrapper').isotope({ filter: tag });
-  }, 700);
-}
-});
-
 
 //#web-applications
 
